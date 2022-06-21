f548ce362f315df514fee41282b804b8efdb6d0f
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-06-21 10:45:53 +0600

    Hospital Patient Generated

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Hospital Management',
+    'version' : '1.0.0',
+    'summary': '',
+    'sequence': -100,
+    'description': """""",
+    'category': '',
+    'website': 'https://www.odoo.com/app/invoicing',
+    'images' : [],
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'license': 'LGPL-3',
+}
---
+++ om_hospital/models/patient.py
@@ -0,0 +1,10 @@
+from odoo import api, models, fields
+
+class HospitalPatient(models.Model):
+    _name = "hopital.patient"
+    _description = "Hospital Patient"
+
+    name = fields.Char(string="Patient Name")
+    Age = fields.Integer(string="Patient Age")
+    gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
+
