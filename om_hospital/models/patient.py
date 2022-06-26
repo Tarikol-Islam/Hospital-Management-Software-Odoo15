0219fcc26f0e3e53c5ba65abe95d5bfda0423705
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-06-26 23:46:11 +0600

    Search View Generated
    Caution:In filter_domain two distincts field is not working properly(mine was failed:Char & Selection)

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -5,6 +5,7 @@ class HospitalPatient(models.Model):
     _description = "Hospital Patient"
 
     name = fields.Char(string="Patient Name")
+    ref = fields.Char(string="Patient Reference")
     age = fields.Integer(string="Patient Age")
     gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
 
