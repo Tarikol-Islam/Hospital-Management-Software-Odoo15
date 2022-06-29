cf58e88f92bbc9bf6774860988b34ab3e26efe05
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-06-29 23:15:29 +0600

    Tracker Configured
    Keeping track of each field mentioned in model. And it's a facility of chatter. If I remove the chatter div from Tree view the tracker also not showing the record

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -5,8 +5,8 @@ class HospitalPatient(models.Model):
     _inherit =[ "mail.thread", "mail.activity.mixin"]
     _description = "Hospital Patient"
 
-    name = fields.Char(string="Patient Name")
-    ref = fields.Char(string="Patient Reference")
+    name = fields.Char(string="Patient Name",tracking=True)
+    ref = fields.Char(string="Patient Reference",tracking=True)
     age = fields.Integer(string="Patient Age")
     gender = fields.Selection([('male','Male'),('female','Female')],string="Gender")
     active = fields.Boolean(string="Active", default=True)
