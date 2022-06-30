7e0c57d69e79c4a9285048f9743795ee6d9c8cf6
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-06-30 11:55:03 +0600

    'Many2one' field initiated
    In creating security problem arise by model_id/id but the problem was I forget to define my model in initiator

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -14,6 +14,7 @@
         'views/menu.xml',
         'views/patient_view.xml',
         'views/female_patient_view.xml',
+        'views/appointment_view.xml',
     ],
     'demo': [],
     'installable': True,
---
+++ om_hospital/models/appointment.py
@@ -0,0 +1,10 @@
+from odoo import api, models, fields
+
+
+class HospitalAppointment(models.Model):
+    _name = "hospital.appointment"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
+    _description = "Hospital Appointment"
+
+    patient_id = fields.Many2one("hospital.patient", string="Patient")
+    date = fields.Datetime(string="Appointment Date")
