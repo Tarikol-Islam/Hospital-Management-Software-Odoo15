49350a4e07221bfa37e27940475eb5cd27525502
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-07-01 10:13:55 +0600

    Notebook installed

--- om_hospital/models/appointment.py
+++ om_hospital/models/appointment.py
@@ -5,9 +5,10 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Hospital Appointment"
+    _rec_name = "patient_id"  # Where 'name' is not a field for the model, then it will works
 
     patient_id = fields.Many2one("hospital.patient", string="Patient")
-    age = fields.Integer(related='patient_id.age')#not editable as its a reference
+    age = fields.Integer(related='patient_id.age')  # not editable as its a reference
     date = fields.Datetime(string="Appointment Date")
     booking_date = fields.Date(string="Booking date", default=fields.Date.context_today)
     ref = fields.Char(string="Reference")
