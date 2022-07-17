c2d42fb7f25e9d173d7c1de927d9799c675c33c9
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-07-17 20:10:20 +0600

    Overriding Copy Method
    We need it when some unique constraing applie for any model

--- om_hospital/models/pharmacy.py
+++ om_hospital/models/pharmacy.py
@@ -13,9 +13,17 @@ class HospitalPharmacy(models.Model):
     int_color = fields.Integer(string="Color")
     hex_color = fields.Char(string="Color")
     appointment_id = fields.Many2one("hospital.appointment", string="Appointment")
+    active = fields.Boolean(string="Active", default=True, copy=False)
 
     _sql_constraints = [
         ('check_times', 'check(times > 0)',
          'You have to take medicine at least one time a day!'),
         ('unique_name', 'unique(name)', "You can't create a tag two or more time")
     ]
+
+    @api.returns('self', lambda value: value.id)
+    def copy(self, default=None):
+        if default is None: default = {}
+        if not default.get('name'):
+            default['name'] = self.name + "-copy"
+        return super(HospitalPharmacy, self).copy(default=default)
